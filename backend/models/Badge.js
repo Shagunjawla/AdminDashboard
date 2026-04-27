f9717abd92b9ce416c430aeb2fadff3180025517
Shagunjawla <shagunjawla12@gmail.com>
2026-04-27 11:37:27 +0530

    initial commit

--- backend/models/Badge.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({
-  name: String,
-  description: String,
-  pointsRequired: Number
-});
-
-module.exports = mongoose.model("Badge", schema);
